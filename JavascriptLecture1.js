e68fcbdf9aa505ef4cdb43df6d5bf2e5dbe244f0
RJcmos <jadhav.rj.cmos@gmail.com>
2023-04-11 23:33:13 +0530

    JS11apr23

---
+++ JavascriptLecture1.js
@@ -0,0 +1,38 @@
+// let a= 10;
+
+// function abc(){
+//     let b=100;
+//     console.log(a);
+//     console.log(b);
+// }
+// console.log(a);
+// abc();
+// console.log(b);   it give you error
+//console.log("==============================");
+// var x= 10;
+
+// function xyz(){
+//     x=200;
+//     var y=100;
+//     console.log(x);
+//     console.log(y);
+// }
+// console.log(x);
+// xyz();
+// console.log(x);
+//console.log(y) ;
+
+
+
+//========
+
+var a= 10;
+console.log(a);
+if(true){
+    var a=12;
+    console.log(a);
+}
+console.log(a);
+
+
+
